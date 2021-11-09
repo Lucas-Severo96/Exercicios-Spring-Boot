22c25eb1d97fd1e1b494312e0a445e6d8298da7b
Lucas Leite Severo <lucasleite_14.1@hotmail.com>
2021-11-09 15:02:24 -0300

    Spring Boot Configurado

---
+++ src/test/java/br/com/cod3r/exerciciosspringb/ExerciciosSpringbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.exerciciosspringb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSpringbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
